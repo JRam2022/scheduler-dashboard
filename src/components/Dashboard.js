53e8d2630211fdb07d8face76e64e669670138f8
JRam2022 <jasonram778@gmail.com>
2022-05-23 16:41:39 -0700

    Create Loading and Panel class

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,59 @@
 import React, { Component } from "react";
 
 import classnames from "classnames";
+import Loading from "./Loading";
+import Panel from "./Panel";
+
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
 
 class Dashboard extends Component {
+  state = {loading: true};
+
   render() {
     const dashboardClasses = classnames("dashboard");
 
-    return <main className={dashboardClasses} />;
-  }
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panel = data.map((p) => (
+        <Panel 
+          key={p.id}
+          id={p.id}
+          label={p.label}
+          value={p.value}
+        />
+      ));
+
+    return (
+      <main className={dashboardClasses}>
+        {panel}
+      </main>
+      )
+    }
 }
 
 export default Dashboard;
