204e714b242ed34ebadd0ee2fe819afc5e8579ee
JRam2022 <jasonram778@gmail.com>
2022-05-23 18:11:40 -0700

    Add Panel states and onclick functions

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -30,27 +30,40 @@ const data = [
 
 
 class Dashboard extends Component {
-  state = {loading: true};
+  state = {
+    loading: false,
+    focused: null
+  };
+
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  }
 
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panel = data.map((p) => (
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+    .map((panel) => (
         <Panel 
-          key={p.id}
-          id={p.id}
-          label={p.label}
-          value={p.value}
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={(event) => this.selectPanel(panel.id)}
         />
       ));
 
     return (
       <main className={dashboardClasses}>
-        {panel}
+        {panels}
       </main>
       )
     }
